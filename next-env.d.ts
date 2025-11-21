ce6abde93b64c237913c315b0b9ac86d0b9dc664
aiandyou50 <me@aiandyou.me>
2025-11-21 14:05:49 +0900

    fix: add global CloudflareEnv type definition

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,6 +5,3 @@
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
 
-interface CloudflareEnv {
-  DB: import("@cloudflare/workers-types").D1Database;
-}
